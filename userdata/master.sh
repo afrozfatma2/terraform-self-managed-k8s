38f8878c0b65bd6320d990459a20a7be71ef7e98
Afroz <afroz00217@gmail.com>
2025-12-09 15:46:13 +0530

    userdata

--- userdata/master.sh
+++ userdata/master.sh
@@ -1,49 +1,102 @@
 #!/bin/bash
 set -e
 
+# ---------------------------
+# Update system
+# ---------------------------
 dnf update -y
 
-# Install Docker
-dnf install -y docker
-systemctl enable docker
-systemctl start docker
-usermod -aG docker ec2-user
+# ---------------------------
+# Install required packages
+# ---------------------------
+dnf install -y tar wget curl git
 
-# Disable swap
+# ---------------------------
+# Disable swap (required for Kubernetes)
+# ---------------------------
 swapoff -a
-sed -i '/ swap / s/^/#/' /etc/fstab
+sed -i '/swap/d' /etc/fstab
 
-# Kubernetes repo
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+# ---------------------------
+# Enable container runtime (CRI-O)
+# ---------------------------
+cat <<EOF | tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
 EOF
 
-# Install kubeadm, kubelet, kubectl
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
+
+# Install CRI-O repo
+OS_VERSION="v1.30"
+CRIO_VERSION="1.30"
+
+curl -L -o /etc/yum.repos.d/libcontainers-stable.repo \
+ https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_Stream_9/devel:kubic:libcontainers:stable.repo
+
+curl -L -o /etc/yum.repos.d/crio.repo \
+ https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/CentOS_Stream_9/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.repo
+
+dnf install -y cri-o
+
+systemctl enable crio
+systemctl start crio
+
+# ---------------------------
+# Kubernetes repo
+# ---------------------------
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/repodata/repomd.xml.key
+EOF
+
+# Install Kubernetes programs
 dnf install -y kubelet kubeadm kubectl
 systemctl enable kubelet
+systemctl start kubelet
 
-# Initialize master
+# ---------------------------
+# Initialize Kubernetes master
+# ---------------------------
 kubeadm init --pod-network-cidr=192.168.0.0/16 | tee /root/kubeinit.txt
 
+# ---------------------------
 # Configure kubectl for ec2-user
+# ---------------------------
 mkdir -p /home/ec2-user/.kube
-cp -i /etc/kubernetes/admin.conf /home/ec2-user/.kube/config
+cp /etc/kubernetes/admin.conf /home/ec2-user/.kube/config
 chown ec2-user:ec2-user /home/ec2-user/.kube/config
 
+# ---------------------------
 # Install Calico network plugin
+# ---------------------------
 sudo -u ec2-user kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
-# Store join command in SSM
+# ---------------------------
+# Create worker join command & store in SSM
+# ---------------------------
 JOIN_CMD=$(kubeadm token create --print-join-command)
+
+dnf install -y awscli
+
 aws ssm put-parameter \
   --name "k8sJoinCommand" \
   --value "$JOIN_CMD" \
   --type String \
   --overwrite \
   --region ap-south-1
+
+echo "Master setup complete."
--- userdata/worker.sh
+++ userdata/worker.sh
@@ -1,35 +1,76 @@
 #!/bin/bash
 set -e
 
+# ---------------------------
+# Update
+# ---------------------------
 dnf update -y
 
-# Install Docker
-dnf install -y docker
-systemctl enable docker
-systemctl start docker
-usermod -aG docker ec2-user
+# Install base packages
+dnf install -y tar wget curl git awscli
 
 # Disable swap
 swapoff -a
-sed -i '/ swap / s/^/#/' /etc/fstab
+sed -i '/swap/d' /etc/fstab
 
-# Kubernetes repo
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+# Enable kernel modules
+cat <<EOF | tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
 EOF
 
-# Install kubeadm, kubelet, kubectl
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
+
+# Install CRI-O
+OS_VERSION="v1.30"
+CRIO_VERSION="1.30"
+
+curl -L -o /etc/yum.repos.d/libcontainers-stable.repo \
+ https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_Stream_9/devel:kubic:libcontainers:stable.repo
+
+curl -L -o /etc/yum.repos.d/crio.repo \
+ https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/CentOS_Stream_9/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.repo
+
+dnf install -y cri-o
+
+systemctl enable crio
+systemctl start crio
+
+# Kubernetes repo
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/repodata/repomd.xml.key
+EOF
+
 dnf install -y kubelet kubeadm kubectl
 systemctl enable kubelet
+systemctl start kubelet
 
-# Fetch join command from SSM
-JOIN_CMD=$(aws ssm get-parameter --name "k8sJoinCommand" --query "Parameter.Value" --output text --region ap-south-1)
+# ---------------------------
+# Download the join command from SSM
+# ---------------------------
+JOIN_CMD=$(aws ssm get-parameter \
+  --name "k8sJoinCommand" \
+  --region ap-south-1 \
+  --query Parameter.Value \
+  --output text)
 
+# ---------------------------
 # Join the cluster
-$JOIN_CMD
+# ---------------------------
+bash -c "$JOIN_CMD"
+
+echo "Worker node joined successfully."
